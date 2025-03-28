f9d9e8bdb783e78f3c7490039cb5466c46ab217b
Dario Alonso Cerezo <contact@darioalonso.com>
2025-03-28 21:33:27 +0000

    Add solution to I18N and apply

--- src/UI/GlobalUsing.cs
+++ src/UI/GlobalUsing.cs
@@ -1,2 +1,4 @@
 global using CommunityToolkit.Maui;
 global using Microsoft.Extensions.Logging;
+global using System.Globalization;
+global using System.ComponentModel;
---
+++ src/UI/TranslateExtension.cs
@@ -0,0 +1,39 @@
+﻿using Binnaculum.Resources.Languages;
+
+namespace Binnaculum;
+
+public class LocalizationResourceManager : INotifyPropertyChanged
+{
+    private LocalizationResourceManager()
+    {
+        AppResources.Culture = CultureInfo.CurrentCulture;
+    }
+    public static LocalizationResourceManager Instance { get; } = new();
+    public object this[string resourceKey] => AppResources.ResourceManager.GetObject(resourceKey, AppResources.Culture) ?? Array.Empty<byte>();
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void SetCulture(CultureInfo culture)
+    {
+        AppResources.Culture = culture;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+    }
+}
+
+[AcceptEmptyServiceProvider]
+[ContentProperty(nameof(Name))]
+public class TranslateExtension : BindableObject, IMarkupExtension<BindingBase>
+{
+    public string? Name { get; set; }
+    public BindingBase ProvideValue(IServiceProvider serviceProvider)
+    {
+        return new Binding
+        {
+            Mode = BindingMode.OneWay,
+            Path = $"[{Name}]",
+            Source = LocalizationResourceManager.Instance
+        };
+    }
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+    {
+        return ProvideValue(serviceProvider);
+    }
+}
