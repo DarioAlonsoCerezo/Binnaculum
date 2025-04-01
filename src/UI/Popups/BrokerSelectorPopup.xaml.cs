cd575d3e2bd1e98277c9530afa6204f9d451d019
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-01 20:10:08 +0100

    Init selecting broker

---
+++ src/UI/Popups/BrokerSelectorPopup.xaml.cs
@@ -0,0 +1,13 @@
+namespace Binnaculum.Popups;
+
+public partial class BrokerSelectorPopup
+{
+	public BrokerSelectorPopup()
+	{
+		InitializeComponent();
+
+        //TODO: Here we should load selectable brokers from the database
+		//TODO: Remove hardcoded brokers after connection
+
+    }
+}
