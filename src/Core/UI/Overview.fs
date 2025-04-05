7106570cf1cd94a53853f1e52430711d9fd3a028
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-05 10:25:26 +0100

    init overview data

---
+++ src/Core/UI/Overview.fs
@@ -0,0 +1,12 @@
+﻿namespace Binnaculum.Core.UI
+
+open System.Reactive.Subjects
+open Binnaculum.Core
+
+module Overview =   
+    
+    let Data = new BehaviorSubject<Models.Home>(Storage.defaulHomeData)
+
+    let Init () =
+        let data = Storage.load<Models.Home> Keys.HomeData Storage.defaulHomeData
+        Data.OnNext data
