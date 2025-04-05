db0ad03362403a2025e6c166e33ed72fe1e40813
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-05 10:25:05 +0100

    Setup storage to save or recover from preferences

---
+++ src/Core/Storage/Storage.fs
@@ -0,0 +1,29 @@
+﻿namespace Binnaculum.Core
+
+open Binnaculum.Core.Models
+open Microsoft.Maui.Storage
+open System.Text.Json
+
+//We will use this to save or load lightweight data serialized from preferences.
+//This is not a database, but a simple key-value storage.
+module internal Storage =
+    
+    let defaulHomeData = {
+        BrokerAccounts = []
+        BankAccounts = []
+        Transactions = []
+    }
+
+    let save dataKey data =        
+        Preferences.Set(dataKey, JsonSerializer.Serialize data)
+
+    let load<'T> (dataKey: string) (defaultValue: 'T) =
+        match Preferences.Get(dataKey, "") with
+        | "" -> defaultValue
+        | json -> JsonSerializer.Deserialize<'T>(json)
+
+    let remove dataKey =
+        Preferences.Remove(dataKey)
+
+    let clear () =
+        Preferences.Clear()
