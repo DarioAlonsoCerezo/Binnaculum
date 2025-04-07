38e0e4d36f68a842a1471caac91cf0ec9d9924b5
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-07 20:20:00 +0100

    Update project files and redefine Keys module
    - Added new F# source files for SQL queries and keys in `Core.fsproj`.
    - Removed reference to `Storage\Keys.fs` and added `Models\CurrencyExtensions.fs`.
    - Replaced the entire `Keys` module in `Keys.fs` with new broker constants while retaining `HomeData`.

---
+++ src/Core/Keys/Keys.fs
@@ -0,0 +1,17 @@
+﻿namespace Binnaculum.Core
+
+module internal Keys =
+    [<Literal>]
+    let Broker_Tastytrade = "Tastytrade"
+
+    [<Literal>]
+    let Broker_Image_Tastytrade = "tastytrade"
+
+    [<Literal>]
+    let Broker_IBKR = "Interactive Brokers"
+
+    [<Literal>]
+    let Broker_Image_IBKR = "ibkr"
+
+    [<Literal>]
+    let HomeData = "home_data"
--- src/Core/Storage/Keys.fs
+++
@@ -1,6 +0,0 @@
-﻿namespace Binnaculum.Core
-
-module internal Keys =
-    [<Literal>]
-    let HomeData = "home_data"
-
