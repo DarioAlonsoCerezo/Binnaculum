6e939be9eef9d03db3c69c066febf1b9d07773fa
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-11 21:58:35 +0100

    Refactor OptionTrade methods for improved clarity
    - Refactored `fill` to use `fillParameters` for better readability.
    - Simplified `save` to utilize `saveEntity`, enhancing code cleanliness.
    - Updated `delete` to call `deleteEntity`, streamlining the process.
    - Modified `getAll` to use `getAllEntities`, simplifying retrieval.
    - Changed `getById` to leverage `getById`, encapsulating fetch logic.

--- src/Core/Models/OptionTradeExtensions.fs
+++ src/Core/Models/OptionTradeExtensions.fs
@@ -5,31 +5,33 @@ open Binnaculum.Core.Database.DatabaseModel
 open Microsoft.Data.Sqlite
 open Binnaculum.Core.Database.TypeParser
 open Binnaculum.Core
-open Binnaculum.Core.SQL
 open DataReaderExtensions
+open CommandExtensions
 
     [<Extension>]
     type Do() =
 
         [<Extension>]
         static member fill(optionTrade: OptionTrade, command: SqliteCommand) =
-            command.Parameters.AddWithValue("@Id", optionTrade.Id) |> ignore
-            command.Parameters.AddWithValue("@TimeStamp", optionTrade.TimeStamp) |> ignore
-            command.Parameters.AddWithValue("@ExpirationDate", optionTrade.ExpirationDate) |> ignore
-            command.Parameters.AddWithValue("@Premium", optionTrade.Premium) |> ignore
-            command.Parameters.AddWithValue("@NetPremium", optionTrade.NetPremium) |> ignore
-            command.Parameters.AddWithValue("@TickerId", optionTrade.TickerId) |> ignore
-            command.Parameters.AddWithValue("@BrokerAccountId", optionTrade.BrokerAccountId) |> ignore
-            command.Parameters.AddWithValue("@CurrencyId", optionTrade.CurrencyId) |> ignore
-            command.Parameters.AddWithValue("@OptionType", fromOptionTypeToDatabase optionTrade.OptionType) |> ignore
-            command.Parameters.AddWithValue("@Code", fromOptionCodeToDatabase optionTrade.Code) |> ignore
-            command.Parameters.AddWithValue("@Strike", optionTrade.Strike) |> ignore
-            command.Parameters.AddWithValue("@Commissions", optionTrade.Commissions) |> ignore
-            command.Parameters.AddWithValue("@Fees", optionTrade.Fees) |> ignore
-            command.Parameters.AddWithValue("@IsOpen", optionTrade.IsOpen) |> ignore
-            command.Parameters.AddWithValue("@ClosedWith", optionTrade.ClosedWith) |> ignore
-            command
-
+            command.fillParameters(
+                [
+                    ("@Id", optionTrade.Id);
+                    ("@TimeStamp", optionTrade.TimeStamp);
+                    ("@ExpirationDate", optionTrade.ExpirationDate);
+                    ("@Premium", optionTrade.Premium);
+                    ("@NetPremium", optionTrade.NetPremium);
+                    ("@TickerId", optionTrade.TickerId);
+                    ("@BrokerAccountId", optionTrade.BrokerAccountId);
+                    ("@CurrencyId", optionTrade.CurrencyId);
+                    ("@OptionType", fromOptionTypeToDatabase optionTrade.OptionType);
+                    ("@Code", fromOptionCodeToDatabase optionTrade.Code);
+                    ("@Strike", optionTrade.Strike);
+                    ("@Commissions", optionTrade.Commissions);
+                    ("@Fees", optionTrade.Fees);
+                    ("@IsOpen", optionTrade.IsOpen);
+                    ("@ClosedWith", optionTrade.ClosedWith)
+                ])
+            
         [<Extension>]
         static member read(reader: SqliteDataReader) =
             { 
@@ -51,34 +53,11 @@ open DataReaderExtensions
             }
 
         [<Extension>]
-        static member save(optionTrade: OptionTrade) = task {
-            let! command = Database.Do.createCommand()
-            command.CommandText <- 
-                match optionTrade.Id with
-                | 0 -> OptionsQuery.insert
-                | _ -> OptionsQuery.update
-            do! command.ExecuteNonQueryAsync() |> Async.AwaitTask |> Async.Ignore
-        }
+        static member save(optionTrade: OptionTrade) = Database.Do.saveEntity optionTrade (fun t c -> t.fill c) 
 
         [<Extension>]
-        static member delete(optionTrade: OptionTrade) = task {
-            let! command = Database.Do.createCommand()
-            command.CommandText <- OptionsQuery.delete
-            command.Parameters.AddWithValue("@Id", optionTrade.Id) |> ignore
-            do! command.ExecuteNonQueryAsync() |> Async.AwaitTask |> Async.Ignore
-        }
+        static member delete(optionTrade: OptionTrade) = Database.Do.deleteEntity optionTrade
 
-        static member getAll() = task {
-            let! command = Database.Do.createCommand()
-            command.CommandText <- OptionsQuery.getAll
-            let! optionTrades = Database.Do.readAll<OptionTrade>(command, Do.read)
-            return optionTrades
-        }
+        static member getAll() = Database.Do.getAllEntities Do.read
 
-        static member getById(id: int) = task {
-            let! command = Database.Do.createCommand()
-            command.CommandText <- OptionsQuery.getById
-            command.Parameters.AddWithValue("@Id", id) |> ignore
-            let! optionTrade = Database.Do.readAll<OptionTrade>(command, Do.read)
-            return optionTrade |> List.tryHead
-        }
+        static member getById(id: int) = Database.Do.getById id Do.read
