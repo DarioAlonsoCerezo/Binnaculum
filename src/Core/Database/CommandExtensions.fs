f9890e7c1532b440f1119c1ed9f6756704f59169
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-13 11:22:20 +0100

    Add XML documentation to CommandExtensions.fs
    Enhanced the `Do` class and its static member functions
    with XML documentation comments. These comments clarify
    the purpose of each function, particularly regarding
    filling commands with parameters and handling entity-related
    parameters.

--- src/Core/Database/CommandExtensions.fs
+++ src/Core/Database/CommandExtensions.fs
@@ -8,18 +8,27 @@ open Binnaculum.Core
     [<Extension>]
     type Do() =
         
+        /// <summary>
+        /// Fills the command with parameters from a list of tuples.
+        /// </summary>
         [<Extension>]
         static member fillParameters(command: SqliteCommand, parameters: (string * obj) list) =
             for (key, value) in parameters do
                 command.Parameters.AddWithValue(key, box value) |> ignore
             command
 
+        /// <summary>
+        /// Fills the command with parameters from a list of tuples, and adds an Id parameter.
+        /// </summary>
         [<Extension>]
         static member fillEntity<'a when 'a :> IEntity>(command: SqliteCommand, parameters: (string * obj) list) =
             command.fillParameters(parameters)
             |> fun c -> c.Parameters.AddWithValue(SQLParameterName.Id, box Unchecked.defaultof<'a>.Id) |> ignore
             command
 
+        /// <summary>
+        /// Fills the command with parameters from a list of tuples, and adds CreatedAt and UpdatedAt parameters.
+        /// </summary>
         [<Extension>]
         static member fillAuditable<'T when 'T :> IAuditEntity>(command: SqliteCommand, parameters: (string * obj) list) =
             let filled = command.fillParameters(parameters)
@@ -27,6 +36,9 @@ open Binnaculum.Core
             filled.Parameters.AddWithValue(SQLParameterName.UpdatedAt, box Unchecked.defaultof<'T>.UpdatedAt) |> ignore
             filled
 
+        /// <summary>
+        /// Fills the command with parameters from a list of tuples, and adds CreatedAt, UpdatedAt, and Id parameters.
+        /// </summary>
         [<Extension>]
         static member fillEntityAuditable<'a when 'a :> IEntity and 'a :> IAuditEntity>(command: SqliteCommand, parameters: (string * obj) list) =
             command.fillEntity<'a>(parameters)
