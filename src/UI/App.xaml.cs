6377d0170a34ba7e509d1cd57d00bcf3f576cd58
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-13 17:18:54 +0100

    Add startup time logging to App.xaml.cs
    Introduce a Stopwatch to measure application startup time.
    Add methods to initialize and log the elapsed time.
    Modify OnStart method to include base call.

--- src/UI/App.xaml.cs
+++ src/UI/App.xaml.cs
@@ -1,7 +1,10 @@
-﻿namespace Binnaculum;
+﻿using System.Diagnostics;
+
+namespace Binnaculum;
 
 public partial class App : Application
 {
+    private static readonly Stopwatch StartupStopwatch = new Stopwatch();
     public App()
     {
         InitializeComponent();
@@ -19,10 +22,24 @@ public partial class App : Application
     protected override void OnStart()
     {
         base.OnStart();
+        
     }
 
     protected override void OnResume()
     {
         base.OnResume();
+    }
+
+    public static void IniLogStartupTime()
+    {
+        // Start the stopwatch when the app is initialized
+        StartupStopwatch.Start();
+    }
+
+    public static void LogStartupTime()
+    {
+        // Stop the stopwatch and log the elapsed time
+        StartupStopwatch.Stop();
+        Debug.WriteLine($"App startup time: {StartupStopwatch.ElapsedMilliseconds} ms");
     }
 }
