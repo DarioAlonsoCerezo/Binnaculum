fb2c6b44c4939a82441c6e22972aa6769f6647c1
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-13 17:44:03 +0100

    Add UI components for collections and model management
    Updated `Core.fsproj` to include `Collections.fs` and `ModelUI.fs`. Introduced the `Binnaculum.Core.UI` namespace with `SourceList` instances for brokers and currencies. Implemented functions in `ModelUI.fs` for converting database-supported brokers and loading data. Modified `InitDatabase` in `Overview.fs` to utilize the new `initialize` function from `ModelUI`.

---
+++ src/Core/UI/Collections.fs
@@ -0,0 +1,8 @@
+﻿namespace Binnaculum.Core.UI
+
+open DynamicData
+open Binnaculum.Core.Models
+
+module Collections =
+    let Brokers = new SourceList<Broker>()
+    let Currencies = new SourceList<Currency>()
---
+++ src/Core/UI/ModelUI.fs
@@ -0,0 +1,48 @@
+﻿namespace Binnaculum.Core.UI
+
+open Binnaculum.Core
+open Binnaculum.Core.Models
+open DynamicData
+
+module internal ModelUI =
+    let toModelSupportedBroker(databaseSupportedBroker: Database.DatabaseModel.SupportedBroker) =
+        match databaseSupportedBroker with
+        | Database.DatabaseModel.SupportedBroker.IBKR -> SupportedBroker.IBKR
+        | Database.DatabaseModel.SupportedBroker.Tastytrade -> SupportedBroker.Tastytrade
+
+    let private loadCurrencies() = task {
+        let! databaseCurrencies = CurrencyExtensions.Do.getAll() |> Async.AwaitTask
+        let currencies =
+            databaseCurrencies 
+            |> List.map (fun c -> 
+            { 
+                Id = c.Id; 
+                Name = c.Name; 
+                Code = c.Code;
+                Symbol = c.Symbol 
+            })
+        Collections.Currencies.EditDiff currencies
+    }
+
+    let private loadBrokers() = task {
+        let! databaseBrokers = BrokerExtensions.Do.getAll() |> Async.AwaitTask
+        let brokers =
+            databaseBrokers 
+            |> List.map (fun b -> 
+            { 
+                Id = b.Id; 
+                Name = b.Name; 
+                Image = b.Image; 
+                SupportedBroker = toModelSupportedBroker b.SupportedBroker
+            })
+
+        Collections.Brokers.EditDiff brokers
+    }
+
+    let initialize() = task {
+        do! Database.Do.init() |> Async.AwaitTask |> Async.Ignore
+        do! BrokerExtensions.Do.insertIfNotExists() |> Async.AwaitTask |> Async.Ignore
+        do! CurrencyExtensions.Do.insertDefaultValues() |> Async.AwaitTask |> Async.Ignore
+        do! loadCurrencies() |> Async.AwaitTask |> Async.Ignore
+        do! loadBrokers() |> Async.AwaitTask |> Async.Ignore
+    }
