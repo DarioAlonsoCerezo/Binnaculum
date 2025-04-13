1f1dabf1811a93a5386b2a27711e0b3300dda81b
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-13 17:50:47 +0100

    Add documentation for ModelUI module
    Enhanced the `ModelUI` module with a summary comment that describes its role in managing data transformation and synchronization between database and UI models. The documentation clarifies the module's responsibilities in loading, creating, and modifying UI models while maintaining decoupling from the database structure. It also indicates plans for future expansion to support additional features.

--- src/Core/UI/ModelUI.fs
+++ src/Core/UI/ModelUI.fs
@@ -4,6 +4,13 @@ open Binnaculum.Core
 open Binnaculum.Core.Models
 open DynamicData
 
+/// <summary>
+/// This module serves as a critical layer for managing the transformation and synchronization of data 
+/// between the database models and the UI models. It is responsible for loading, creating, and modifying 
+/// UI models, acting as a proxy to ensure the UI remains decoupled from the database structure. 
+/// As the application evolves, this module will expand to support additional features, providing a 
+/// centralized and scalable approach to handling UI data.
+/// </summary>
 module internal ModelUI =
     let toModelSupportedBroker(databaseSupportedBroker: Database.DatabaseModel.SupportedBroker) =
         match databaseSupportedBroker with
