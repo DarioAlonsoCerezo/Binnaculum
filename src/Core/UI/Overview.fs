e3c3a12d660827a1f849617b04876fbd7612cad2
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-16 18:06:16 +0100

    Add data loading and import for storage module
    - Added import for `Binnacle.Core.Storage` in `Overview.fs`.
    - Updated `InitDatabase` to load basic data using `DataLoader.loadBasicData()` instead of initializing `overview` from `ModelUI.initialize(Data.Value)`.
    - Updated the `Data` subject to reflect that the database has been initialized after loading data.

--- src/Core/UI/Overview.fs
+++ src/Core/UI/Overview.fs
@@ -2,14 +2,15 @@
 
 open System.Reactive.Subjects
 open Binnaculum.Core.Models
+open Binnacle.Core.Storage
 
 module Overview = 
 
     let Data = new BehaviorSubject<OverviewUI>({ IsDatabaseInitialized = false; TransactionsLoaded = false});
 
     let InitDatabase() = task {
-        let! overview = ModelUI.initialize(Data.Value)
-        Data.OnNext overview
+        do! DataLoader.loadBasicData() |> Async.AwaitTask |> Async.Ignore
+        Data.OnNext { Data.Value with IsDatabaseInitialized = true; }
     }
 
     let LoadData() = task {
