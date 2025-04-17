56a12e4afdb892e0045ac1f6742629d279fcf89a
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-17 20:24:13 +0100

    Add broker selection UI with Expander control
    Updated `AccountCreatorPage.xaml` to include a new `toolkit` namespace and modified the layout to enhance user experience. Introduced an `Expander` control for broker selection, replacing the previous display elements.
    
    In `AccountCreatorPage.xaml.cs`, added logic to handle broker selection events and manage UI state. Introduced a new method `SetSelection` to update the visibility of selection elements based on user interaction.

--- src/UI/Pages/AccountCreatorPage.xaml.cs
+++ src/UI/Pages/AccountCreatorPage.xaml.cs
@@ -1,8 +1,10 @@
+using Binnaculum.Core.UI;
+
 namespace Binnaculum.Pages;
 
 public partial class AccountCreatorPage
 {
-	public AccountCreatorPage()
+    public AccountCreatorPage()
 	{
 		InitializeComponent();
     }    
@@ -16,5 +18,40 @@ public partial class AccountCreatorPage
         ButtonSaveOrDiscard.Events().DiscardClicked
             .Select(async _ => await Navigation.PopModalAsync())
             .Subscribe();
+
+        var ibkr = Collections.Brokers.Items.Single(x => x.SupportedBroker == "Interactive Brokers");
+        var tasty = Collections.Brokers.Items.Single(x => x.SupportedBroker == "Tastytrade");
+        if (ibkr != null)
+            IBKR.Broker = ibkr;
+        if (tasty != null)
+            Tastytrade.Broker = tasty;
+
+        Tastytrade.Events().BrokerSelected
+            .Do(SetSelection)
+            .Subscribe()
+            .DisposeWith(Disposables);
+
+        IBKR.Events().BrokerSelected
+            .Do(SetSelection)
+            .Subscribe()
+            .DisposeWith(Disposables);
+
+        SelectedBroker.Events().BrokerSelected
+            .Do(x =>
+            {
+                ExpanderTitle.IsVisible = true;
+                SelectedBroker.IsVisible = false;
+                BrokerExpander.IsExpanded = true;
+            })
+            .Subscribe()
+            .DisposeWith(Disposables);
+    }
+
+    private void SetSelection(Core.Models.Broker broker)
+    {
+        SelectedBroker.Broker = broker;
+        ExpanderTitle.IsVisible = false;
+        SelectedBroker.IsVisible = true;
+        BrokerExpander.IsExpanded = false;
     }
 }
