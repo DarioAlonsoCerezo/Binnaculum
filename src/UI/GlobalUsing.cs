2dcf891a112a886356e619e8f177e382a205d6b9
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-17 19:30:25 +0100

    Update global usings for reactive programming support
    Added namespaces for System.Collections.ObjectModel, ReactiveUI, and DynamicData. Removed the System namespace to streamline imports and enhance data binding capabilities within a reactive context.

--- src/UI/GlobalUsing.cs
+++ src/UI/GlobalUsing.cs
@@ -7,5 +7,8 @@ global using Binnaculum.Extensions;
 global using Binnaculum.Resources.Languages;
 global using System.Reactive.Disposables;
 global using System.Reactive.Concurrency;
+global using System.Collections.ObjectModel;
 global using System;
 global using ReactiveUI;
+global using DynamicData;
+global using DynamicData.Binding;
