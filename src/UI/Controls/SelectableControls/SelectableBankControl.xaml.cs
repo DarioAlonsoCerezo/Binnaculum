c60aab62014f5a6308259eff7580892795d30b26
Dario Alonso Cerezo <contact@darioalonso.com>
2025-04-27 12:18:38 +0100

    Add SelectableBankControl UI component and logic
    Introduce a new XAML file for the SelectableBankControl, featuring a layout with an icon and bank name label, along with a tap gesture recognizer. Update the code-behind to implement a bindable Bank property, an event for bank selection, and data binding for the bank's image and name.

---
+++ src/UI/Controls/SelectableControls/SelectableBankControl.xaml.cs
@@ -0,0 +1,47 @@
+namespace Binnaculum.Controls;
+
+public partial class SelectableBankControl
+{
+    public event EventHandler<Core.Models.Bank>? BankSelected;
+
+    public static readonly BindableProperty BankProperty = BindableProperty.Create(
+        nameof(Bank),
+        typeof(Core.Models.Bank),
+        typeof(SelectableBankControl),
+        default(Core.Models.Bank));
+
+    public Core.Models.Bank Bank
+    {
+        get => (Core.Models.Bank)GetValue(BankProperty);
+        set => SetValue(BankProperty, value);
+    }
+
+    public SelectableBankControl()
+	{
+		InitializeComponent();
+	}
+
+    protected override void StartLoad()
+    {
+        ContentGesture.Events().Tapped
+            .Where(_ => Bank != null)
+            .Subscribe(_ =>
+            {
+                BankSelected?.Invoke(this, Bank);
+            }).DisposeWith(Disposables);
+
+        this.WhenAnyValue(x => x.Bank)
+            .WhereNotNull()
+            .Select(x => x.Image)
+            .ObserveOn(UiThread)
+            .BindTo(BankImage, x => x.ImagePath)
+            .DisposeWith(Disposables);
+
+        this.WhenAnyValue(x => x.Bank)
+            .WhereNotNull()
+            .Select(x => x.Name)
+            .ObserveOn(UiThread)
+            .BindTo(BankName, x => x.Text)
+            .DisposeWith(Disposables);
+    }
+}
