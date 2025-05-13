e08555b50a1fdf22ff3e53330ab9210e8bb0cd47
Dario Alonso Cerezo <contact@darioalonso.com>
2025-05-13 21:18:01 +0100

    Enhance BrokerMovementDepositControl with event handling
    Updated XAML to include x:Name attributes for key controls.
    Introduced DepositChanged event and _deposit field in the
    code-behind. Enhanced StartLoad method to subscribe to
    UI control events for updating deposit information.
    Added DepositControl record type to encapsulate deposit
    state properties.

--- src/UI/Controls/BrokerMovementControls/BrokerMovementDepositControl.xaml.cs
+++ src/UI/Controls/BrokerMovementControls/BrokerMovementDepositControl.xaml.cs
@@ -2,6 +2,10 @@ namespace Binnaculum.Controls;
 
 public partial class BrokerMovementDepositControl
 {
+    public event EventHandler<DepositControl> DepositChanged;
+
+    private DepositControl _deposit;
+
 	public BrokerMovementDepositControl()
 	{
 		InitializeComponent();
@@ -9,6 +13,47 @@ public partial class BrokerMovementDepositControl
 
     protected override void StartLoad()
     {
-        
+        AmountEntry.Events().CurrencyChanged
+            .Subscribe(x =>
+            {
+                _deposit = _deposit with { Currency = x };
+
+                DepositChanged?.Invoke(this, _deposit);
+            })
+            .DisposeWith(Disposables);
+
+        AmountEntry.Events().TextChanged
+            .Subscribe(x =>
+            {
+                if (decimal.TryParse(x.NewTextValue, out var amount))
+                {
+                    _deposit = _deposit with { Amount = amount };
+                    DepositChanged?.Invoke(this, _deposit);
+                }
+            })
+            .DisposeWith(Disposables);
+
+        DateTimePicker.Events().DateSelected
+            .Subscribe(x =>
+            {
+                _deposit = _deposit with { TimeStamp = x };
+                DepositChanged?.Invoke(this, _deposit);
+            })
+            .DisposeWith(Disposables);
+
+        FeesAndCommissions.Events().FeeAndCommissionChanged
+            .Subscribe(x =>
+            {
+                _deposit = _deposit with { Commissions = x.Commission, Fees = x.Fee };
+                DepositChanged?.Invoke(this, _deposit);
+            })
+            .DisposeWith(Disposables);
     }
 }
+
+public record DepositControl(
+    DateTime TimeStamp, 
+    decimal Amount, 
+    string Currency,
+    decimal Commissions,
+    decimal Fees);
