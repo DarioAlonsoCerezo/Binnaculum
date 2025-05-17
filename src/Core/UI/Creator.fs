91c02338ba166e29057c67a36fb72945a3b175bd
Dario Alonso Cerezo <contact@darioalonso.com>
2025-05-17 18:22:30 +0100

    allow database do the update change

--- src/Core/UI/Creator.fs
+++ src/Core/UI/Creator.fs
@@ -67,8 +67,7 @@ module Creator =
         let! bank = BankExtensions.Do.getById bankId |> Async.AwaitTask
         match bank with
         | Some b ->
-            let updatedAt = DateTimePattern.FromDateTime DateTime.Now
-            let audit = { b.Audit with UpdatedAt = Some updatedAt }
+            let audit = { b.Audit with UpdatedAt = None }
             let bank = { b with Image = Some iconPath; Audit = audit }
             do! bank.save() |> Async.AwaitTask |> Async.Ignore
             do! DataLoader.getOrRefreshAllAccounts() |> Async.AwaitTask |> Async.Ignore
