4c320acb5543d380b602993cf1c16829e2cc9fdd
Dario Alonso Cerezo <contact@darioalonso.com>
2025-05-26 14:33:26 +0100

    Refactor OptionTradeBuilder layout and functionality
    Updated OptionTradeBuilder.xaml to use VerticalStackLayout with radio buttons for "CALL" and "PUT" options. Enhanced interactivity in OptionTradeBuilder.xaml.cs by adding event subscriptions to manage radio button states. Modified OptionTradeControl.xaml to include multiple instances of OptionTradeBuilder for different legs and replaced the BorderedDateTimePickerControl.

--- src/UI/Controls/BrokerMovementControls/OptionTradeBuilder.xaml.cs
+++ src/UI/Controls/BrokerMovementControls/OptionTradeBuilder.xaml.cs
@@ -9,6 +9,14 @@ public partial class OptionTradeBuilder
 
     protected override void StartLoad()
     {
-        
+        PutRadioButton.Events().CheckedChanged
+            .Where(x => x)
+            .Subscribe(_ => CallRadioButton.IsChecked = false)
+            .DisposeWith(Disposables);
+
+        CallRadioButton.Events().CheckedChanged
+            .Where(x => x)
+            .Subscribe(_ => PutRadioButton.IsChecked = false)
+            .DisposeWith(Disposables);
     }
 }
