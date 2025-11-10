20b67b6494d6a0a3bc38b3157ab24d9eb45c8821
Dario Alonso Cerezo <contact@darioalonso.com>
2025-11-10 16:02:57 +0000

    Add CoreExtensions with methods for CurrentImportStatus handling

---
+++ src/UI/Extensions/CoreExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Binnaculum.Core.Import;
+
+namespace Binnaculum.Extensions;
+
+public static class CoreExtensions
+{
+    public static bool ToEnableButton(this CurrentImportStatus status)
+    {
+        var state = status.State;
+        return state == ImportStateEnum.NotStarted ||
+            state == ImportStateEnum.Cancelled ||
+            state == ImportStateEnum.Completed ||
+            state == ImportStateEnum.Failed;
+    }
+
+    public static bool ToShowProgress(this CurrentImportStatus status)
+    {
+        return !status.ToEnableButton();
+    }
+
+    public static bool ToShowResults(this CurrentImportStatus status)
+    {
+        var state = status.State;
+        return state != ImportStateEnum.NotStarted;
+    }
+}
